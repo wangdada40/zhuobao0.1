302989ec8233388de2fb2a0ea1652168f71a4454
wangdada40 <43258773+wangdada40@users.noreply.github.com>
2019-03-10 23:56:28 +0800

    对接后台

--- 珠宝/Controllers/GuideController.cs
+++ 珠宝/Controllers/GuideController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using 珠宝.App_Start;
 
 namespace 珠宝.Controllers
 {
@@ -17,7 +20,54 @@ namespace 珠宝.Controllers
 
             return View();
         }
+        //上传喜好数据
 
+        public string UpdateAnimation(int id,string style,string color,string describer,int money) {
+            if (IsUser(id))
+            {
+                DataConnect sqlData = new DataConnect();
+                String sql = "INSERT INTO UserLove(userID,style,color,describer,money) VALUES('" + id + "','" + "大众风" + "','" + color + "','" + describer + "','" + money + "')";
+                sqlData.Update(sql);
+                return "上传成功";
+            }
+            else {
+                DataConnect sqlData = new DataConnect();
+                String sql = "UPDATE UserLove SET style = '"+style+ "', color = '" + color + "',describer ='"+describer+"',money="+money+"' WHERE userID = 'Wilson'";
+                sqlData.Check(sql);
+                return "上传成功";
+            }
+          
+            
+        }
 
+        //查找喜好数据
+        public ActionResult GetAnimation(int id)
+        {
+            DataConnect sqlData = new DataConnect();
+            String sql = "select * from UserLove where userID='" + id + "'";
+            //接收数据
+            DataSet data = sqlData.Check(sql);
+            var checkjson = data.Tables[0];
+            return Content(JsonConvert.SerializeObject(checkjson));
+
+        }
+
+        //查找是否存在用户
+        public bool IsUser(int id)
+        {
+            DataConnect sqlData = new DataConnect();
+            String sql = "select * from UserLove where userID='" + id + "'";
+            //接收数据
+            DataSet data = sqlData.Check(sql);
+            if (data.Tables[0].Rows.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
     }
 }
